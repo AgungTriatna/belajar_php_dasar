e672db7950268fb2e3280521fe5191c19c353e0c
AgungTriatna <105510687+AgungTriatna@users.noreply.github.com>
2023-01-19 00:11:04 +0700

    Belajar hari ke-1

---
+++ f_Constant.php
@@ -0,0 +1,12 @@
+<?php
+
+define("AUTHOR", "Agung Triatna");
+define('APP_VERSION', 100);
+
+echo AUTHOR;
+echo "\n";
+echo APP_VERSION;
+echo "\n";
+
+
+//Materi selanjutnya Data null -> https://www.youtube.com/watch?v=TaBWhb5SPfc&t=3874s
